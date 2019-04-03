68376f743aaa8ef5c3bed643ba4d0b873f519e62
Airat Halitov <airat.halitov@gmail.com>
2019-04-03 13:57:43 +0500

    Create vk-subs-clean.js

---
+++ vk/vk-subs-clean.js
@@ -0,0 +1,27 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
+
+(function() {
+    'use strict';
+    // Запускать на странице https://vk.com/dev/account.ban
+    // Все добавлятся в ЧС, потом через пару часов используйте vk-blacklist-clean.js
+    
+    // Список айдишников пользователей через запятую
+    var mylist = [1,2,3];
+    
+    if (!confirm('Delete ' + mylist.length + '?')) return;
+    var formField = document.body.querySelector('#dev_const_owner_id');
+    var buttonLink = document.body.querySelector('#dev_req_run_btn');
+    for (var i = 0; i < mylist.length; i++) {
+        formField.value = mylist[i];
+        buttonLink.click();
+        sleep(2000); // задержка 2 секунды между запросами
+    }
+    alert(mylist.length + ' user deleted');
+}());
