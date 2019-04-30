d86503f7c892d9ce40cad22423465f5455d3ba89
Airat Halitov <4050715+AiratHalitov@users.noreply.github.com>
2019-04-30 19:57:02 +0500

    Create vk-out-requests-clean.js

---
+++ vk/vk-out-requests-clean.js
@@ -0,0 +1,11 @@
+// Скрипт для очистки списка исходящих запросов в друзья
+// Запускать на странице с запросами: https://vk.com/friends?section=out_requests (прокрутить страницу вниз до конца)
+(function() {
+    'use strict';
+    var mylist = document.body.querySelectorAll('div.friends_controls > button');
+    if (!confirm('Start: ' + mylist.length + '?')) return;
+    for (var i = 0; i < mylist.length; i++) {
+        mylist[i].click();
+    }
+    alert('Finish: ' + mylist.length + '!');
+}());
